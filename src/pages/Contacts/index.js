5abbb04d1bd2b7703a187ef6a18ffd121de9ed5e
ViktoriiaDanko <danko.vik146@gmail.com>
2021-01-24 13:44:00 +0200

    added lib Material-UI

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,38 +1,20 @@
-import { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles"; //createStyles - theme
+import { useContacts } from "./useContacts";
 
-const useContacts = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch("https://randomuser.me/api?results=200");
-        const { results, error } = await response.json();
-        if (error) {
-          throw new Error(error);
-        }
-        setData(results);
-        setIsError(false);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getContacts();
-  }, []);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(3),
+    },
+  })
+);
 
 export const Contacts = () => {
+  const classes = useStyles();
   const contacts = useContacts();
   if (contacts.isLoading) {
     return <div>...loading</div>;
@@ -42,5 +24,16 @@ export const Contacts = () => {
     return <div>...error</div>;
   }
 
-  return <div>Contacts {contacts.data[0].name.first}</div>;
+  return (
+    <Container className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <div>Contacts</div>
+        </Grid>
+        <Grid item xs={12}>
+          <div>{contacts.data[0].name.first}</div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
